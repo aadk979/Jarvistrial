28cf8fc06bf5d1938aba469d60ad1b07c10c7536
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-10-31 22:29:26 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,63 +1,16 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+
 import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({
     organization: "org-A607kOg9mazKTmUZKRiaMTxo",
     apiKey: "sk-G2yBWV9PjkR8yOXHeHiT3BlbkFJ4TNWG6TO0aliLrBjg3TD",
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
 
-
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "*", 
-        methods: ["GET", "POST"]
-    }
-});
-
-function kill(){
-  const axios = require('axios');
-
-	const options = {
-  	method: 'POST',
-  	url: 'https://api.render.com/v1/services/srv-ckud7cmb0mos738u2ssg/suspend',
-  	headers: {
-    	accept: 'application/json',
-    	authorization: 'Bearer rnd_dbjVtRsFHMVGqUPbdHtlPLN4ulbq'
-  	}
-	};
-
-	axios
-  	.request(options)
-  	.then(function (response) {
-    	console.log(response.data);
-      io.emit("server" , "server shut down succesfully");
-  	})
-  	.catch(function (error) {
-    	console.error(error);
-  	});
-}
-
-
-io.on("connection", (socket) => {
-    socket.on("server-kill" , (data) =>{
-      if (data.akc === "I like amelie" && data.acc === "260908180608"){
-       
-      }else{
-        io.emit("server" , "Authentication failed , unable to shutdown server");
-      }
-    });
-});
-
-const PORT = process.env.PORT || 5500;
-
-server.listen(PORT, () => {
-    console.log(`Server is up and running, server listening on port ${PORT}.`);
-});
+const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [
+			{role: "user", content: "define javascript"},
+})
+            
+console. log (completion.data.choices[0].message);
